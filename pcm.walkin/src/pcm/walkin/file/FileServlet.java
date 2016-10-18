b3e70e328b67dcea2d7f0ff2beae2c88fbc06346
ChanMinPark <walkinpcm@gmail.com>
2016-10-18 22:08:42 +0900

    #9

---
+++ pcm.walkin/src/pcm/walkin/file/FileServlet.java
@@ -0,0 +1,62 @@
+package pcm.walkin.file;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+@WebServlet("/file/*")
+public class FileServlet extends HttpServlet {
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/plain;charset=UTF-8");
+		
+		PrintWriter out = resp.getWriter();
+		out.println("File Post 요청에 대한 응답입니다.");
+	}
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/plain;charset=UTF-8");
+		
+		PrintWriter out = resp.getWriter();
+		out.println("File Get 요청에 대한 응답입니다.");
+	}
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doPut(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/plain;charset=UTF-8");
+		
+		PrintWriter out = resp.getWriter();
+		out.println("File Put 요청에 대한 응답입니다.");
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doDelete(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/plain;charset=UTF-8");
+		
+		PrintWriter out = resp.getWriter();
+		out.println("File Delete 요청에 대한 응답입니다.");
+	}
+
+}
